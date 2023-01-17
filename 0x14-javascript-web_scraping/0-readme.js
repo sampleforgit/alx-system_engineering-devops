60103a04337f38a226fe6a2b5a881342dd23077c
root <dmercy2015@gmail.com>
2023-01-17 06:15:12 -0800

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
-const filePath = process.argv[2];
 const fs = require('fs');
-fs.readFile(filePath, 'utf8', function (err, data) {
+const argms = process.argv[2];
+
+fs.readFile(argms, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(data.toString());
   }
 });
